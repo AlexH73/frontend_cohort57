28b8f40af47e0d6357e0e477acb52f59a2e2b9dd
Alex_H <ewebotah@gmail.com>
2025-07-20 03:29:00 +0200

    update: Подправил мелочи

--- lessons/lesson_17/code/fake-store-app/js/profile.js
+++ lessons/lesson_17/code/fake-store-app/js/profile.js
@@ -1,6 +1,7 @@
 const heading = document.getElementById("heading");
 const img = document.getElementById("avatar");
 const emailEl = document.getElementById("email");
+const roleEl = document.getElementById("role");
 
 async function fetchProfile() {
   const token = localStorage.getItem("accessToken");
@@ -10,10 +11,13 @@ async function fetchProfile() {
 
   const user = await res.json();
   console.log(user);
-  const { avatar, email, name } = user;
+  const { avatar, email, name, role } = user;
   heading.textContent = name;
   img.src = avatar;
+  img.alt = `Avatar of ${name}`;
+  img.title = `Avatar of ${name}`;
   emailEl.textContent = email;
+  roleEl.textContent = role;
 }
 
 fetchProfile();
